64904750ecdb5a440c7c69aebd25f9caf34f25c5
Laura Johnston <laura.s.johnston@disney.com>
2023-09-07 13:55:11 -0700

    adding new code from today's class

--- demo/pizza2.py
+++ demo/pizza2.py
@@ -1,6 +1,8 @@
 """ write a program which takes a pizza order, and prints out
 instructions for the kitchen to follow. """
 
+# this global variable contains the unicode codes for the start of the symbols
+# that will be used for choosing the different categories of ingredient
 OPTION_START = {'dough': 33, 'sauces': 49, 'toppings': 65}
 
 def get_ingredients():
@@ -33,63 +35,37 @@ def get_ingredients():
 
 
 def print_menu(ingredients):
-    """ print out a menu with numbers to make it easy to order, and easy to
-    double-check the order makes sense """
+    """ print out a menu with numbers """
 
-    # start with dough, using symbols
-    print('Select a dough option (limit 1):')
-    for i, option in enumerate(ingredients['dough']):
-        symbol = chr(i + OPTION_START['dough']) # this starts with the ! symbol
-        print(f'{symbol}: {option}')
-
-    print('Select a sauce option (limit 1):')
-    for i, option in enumerate(ingredients['sauces']):
-        symbol = chr(i + OPTION_START['sauces']) # this starts with 1
-        print(f'{symbol}: {option}')
-
-    print('Select your toppings (no limit):')
-    for i, option in enumerate(ingredients['toppings']):
-        symbol = chr(i + OPTION_START['toppings']) # this starts with A
-        print(f'{symbol}: {option}')
-
-    print('A valid response might look like: !2EFA')
+    for i, option in enumerate(ingredients):
+        print(f'{i+1}: {option}')
 
 
 def get_response(ingredients):
-    """ prompt the user to input a string representing a pizza.
-    double-check that the ingredients make sense (one dough, one sauce,
-    zero or more toppings) """
-    response = input('Enter your pizza code: ')
+    response = int(input('What is your choice? '))
+    max_num = len(ingredients)
+    if response > 0 and response <= max_num:
+        return response
 
-    def check_ingredient(ingredient_type, max_i=10, min_i=1):
-        # first, check dough
-        upper_limit = len(ingredients[ingredient_type])
-        code_space = [
-                      OPTION_START[ingredient_type],
-                      OPTION_START[ingredient_type] + upper_limit
-                     ]
-        num_items = 0
-        for letter in response:
-            if ord(letter) >= code_space[0] and ord(letter) <= code_space[1]:
-                num_items += 1
-        if num_items < min_i or num_items > max_i:
-            print(f'Please select *min/max this* {ingredient_type} option')
-
-    check_ingredient('dough', max_i=1)
-
-
-def print_order():
-    pass
+def print_order(response, ingredients):
+    print('Dear kitchen, please make the following pizza:')
+    for item in ingredients:
+        ingredient_num = response[item] - 1
+        print(f'{item}: {ingredients[item][ingredient_num]}')
 
 def main():
     ingredients = get_ingredients()
     #print_menu(ingredients)
-    while True:
-        print_menu(ingredients)
-        response = get_response(ingredients)
-        if response:
-            break
-    print_order(response)
+    kitchen_response = {}
+    for item in ingredients:
+        while True:
+            print(f'Select your {item}')
+            print_menu(ingredients[item])
+            response = get_response(ingredients[item])
+            if response:
+                break
+        kitchen_response[item] = response
+    print_order(kitchen_response, ingredients)
 
 if __name__ == '__main__':
     main()
